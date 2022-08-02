1c5c904644e2976bfcf03e55de0b84d5793afe3b
huolu <richie_h@163.com>
2022-08-02 14:36:14 +0800

    webpack-mock-server-plugin

---
+++ .history/Watcher_20220802113316.js
@@ -0,0 +1,30 @@
+class Watcher extends event.EventEmitter {
+  constructor(watchDir) {
+    super();
+    this.watchDir = watchDir;
+    this.watchList = new Set();
+  }
+
+  watch() {
+    this.watchList.forEach((value) => {
+      fs.unwatchFile(value);
+      fs.watch(value, { persistent: false, recursive: true }, (event, filename) => {
+        console.log(chalk.yellow("mock data changed", filename));
+        this.emit("process", filename);
+      });
+    });
+  }
+
+  addFileDir(watchDir) {
+    if (fs.lstatSync(watchDir).isDirectory()) {
+      this.watchList.add(`${watchDir}`);
+    }
+  }
+
+  start() {
+    this.addFileDir(this.watchDir);
+    this.watch();
+  }
+}
+
+module.exports = Watcher;
---
+++ .history/Watcher_20220802114505.js
@@ -0,0 +1,33 @@
+const event = require("events");
+const fs = require("fs");
+
+class Watcher extends event.EventEmitter {
+  constructor(watchDir) {
+    super();
+    this.watchDir = watchDir;
+    this.watchList = new Set();
+  }
+
+  watch() {
+    this.watchList.forEach((value) => {
+      fs.unwatchFile(value);
+      fs.watch(value, { persistent: false, recursive: true }, (event, filename) => {
+        console.log(chalk.yellow("mock data changed", filename));
+        this.emit("process", filename);
+      });
+    });
+  }
+
+  addFileDir(watchDir) {
+    if (fs.lstatSync(watchDir).isDirectory()) {
+      this.watchList.add(`${watchDir}`);
+    }
+  }
+
+  start() {
+    this.addFileDir(this.watchDir);
+    this.watch();
+  }
+}
+
+module.exports = Watcher;
