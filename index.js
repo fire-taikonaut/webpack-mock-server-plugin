d68cf29f3377d5fb368ebabc1c4ac33c4aa35877
huolu <richie_h@163.com>
2022-08-09 11:08:14 +0800

    处理跨域问题

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const compression = require("compression");
+const bodyParser = require("body-parser");
 const Watcher = require("./Watcher");
 
 const appOptions = {
@@ -15,8 +17,7 @@ class WebpackMockServerPlugin {
   }
   apply() {
     this.app = express();
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: false }));
+    this.useMiddleWare();
     this.listen();
   }
   listen() {
@@ -27,6 +28,23 @@ class WebpackMockServerPlugin {
     this.createRouter();
     this.createWather();
     this.subscribeProcess();
+  }
+  useMiddleWare() {
+    this.app.use(compression());
+    this.app.use(bodyParser.json({ limit: "50mb" }));
+    this.app.all("*", function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", req.headers.origin);
+      res.header("Access-Control-Allow-Credentials", "true");
+      res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length , user, Accept, X-Requested-With");
+      res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+      if (req.method == "OPTIONS") {
+        res.sendStatus(200);
+        return;
+      }
+      next();
+    });
+    // this
+    this.app.use(express.urlencoded({ extended: false }));
   }
   isvalid(d) {
     return typeof d === "object" && !Array.isArray(d);
